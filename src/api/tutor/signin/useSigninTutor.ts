45183d664dd74af74881c4180987bf822038204d
ScuBix1 <bastian.monnin1@gmail.com>
2025-05-10 20:09:55 +0200

    tutor-signin: create context to register token and update the mutation and the manage form

--- src/api/tutor/signin/useSigninTutor.ts
+++ src/api/tutor/signin/useSigninTutor.ts
@@ -1,9 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
+import { useAuth } from '../../../context/auth/useAuth';
 import { signinTutor } from './api';
 import { SigninTutorDto } from './schema';
 
 export const useSigninTutor = () => {
+  const { login } = useAuth();
+
   return useMutation({
     mutationFn: (data: SigninTutorDto) => signinTutor(data),
+    onSuccess: (data) => {
+      if ('access_token' in data) {
+        login(data.access_token);
+      }
+    },
   });
 };
--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,22 +1,57 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { SigninTutorDto, signinSchema } from '../../api/tutor/signin/schema';
+import { useSigninTutor } from '../../api/tutor/signin/useSigninTutor';
 import Button from '../../components/Button/Button';
 import InputText from '../../components/InputText/InputText';
 import Link from '../../components/Link/Link';
 
 const Signin = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<SigninTutorDto>({
+    resolver: zodResolver(signinSchema),
+  });
+  const { mutate, error } = useSigninTutor();
+
+  const onSubmit = (data: SigninTutorDto) => {
+    mutate(data);
+  };
+
   return (
     <div className='min-h-[100dvh] w-full'>
       <div className='min-h-[100dvh] flex flex-col justify-center items-center lg:flex-row lg:justify-around'>
         <div className='flex flex-col gap-y-4 justify-center items-center'>
           <h1 className='text-h1'>Se connecter</h1>
-          <form action='' className='flex flex-col gap-y-4 mx-auto'>
-            <InputText id='email' textLabel='E-mail' placeholder='E-mail...' />
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='flex flex-col gap-y-4 mx-auto'
+          >
+            <InputText
+              id='email'
+              textLabel='E-mail'
+              placeholder='E-mail...'
+              {...register('email')}
+              error={errors.email?.message}
+            />
             <InputText
               id='password'
               textLabel='Mot de passe'
               placeholder='Mot de passe...'
+              {...register('password')}
+              error={errors.password?.message}
             />
+            {error && (
+              <span className='h-2 text-red-500 basis-full'>
+                {error.message}
+              </span>
+            )}
+            <Button variant='primary' type='submit'>
+              Se connecter
+            </Button>
           </form>
-          <Button variant='primary'>Se connecter</Button>
           <div className='text-center'>
             <p>Vous n’êtes pas encore inscrit ?</p>
             <Link href='/signup' variant='primary'>
