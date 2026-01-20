7e36e4607346b2e6456ddab99fba8ed91e14e425
ScuBix1 <bastian.monnin1@gmail.com>
2026-01-20 17:05:31 +0100

    verification mail in standby for render free

--- src/api/tutor/signup/useSignupTutor.ts
+++ src/api/tutor/signup/useSignupTutor.ts
@@ -1,18 +1,20 @@
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
-import useSendEmail from '../email/send/useSendEmail';
 import { signupTutor } from './api';
 import { SignupTutorDto } from './schema';
 
+//verification du mail annulé en hébergeant render gratuit (envoie de mail smtp indispo)
 export const useSignupTutor = () => {
   const navigate = useNavigate();
-  const { mutate: emailMutate } = useSendEmail();
+  // const { mutate: emailMutate } = useSendEmail();
 
   return useMutation({
     mutationFn: (data: SignupTutorDto) => signupTutor(data),
-    onSuccess: (_data, variables) => {
-      emailMutate({ email: variables.email });
-      navigate('/email-verification', { state: { email: variables.email } });
+
+    onSuccess: (_data, _variables) => {
+      //   emailMutate({ email: variables.email });
+      // navigate('/email-verification', { state: { email: variables.email } });
+      navigate('/tutor/dashboard');
     },
   });
 };
